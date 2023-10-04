c66b6c6e55988f767d88e67f3a0cde97afa29673
notAxion <67577744+notAxion@users.noreply.github.com>
2023-10-04 06:48:24 +0000

    feat(decode): decoding from response body and returning only the list of BlogModel

--- lib/res/blogs_api.dart
+++ lib/res/blogs_api.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-void fetchBlogs() async {
+Future<List<BlogList>> fetchBlogs() async {
   final String url = 'https://intent-kit-16.hasura.app/api/rest/blogs';
   final String adminSecret = dotenv.env['ADMIN_SECRET'] ?? "";
 
@@ -11,12 +11,14 @@ void fetchBlogs() async {
     });
 
     if (response.statusCode == 200) {
-      // Request successful, handle the response data here
-      print('Response data: ${response.body}');
+      final jsonData = json.decode(response.body);
+      final blogsObject = BlogsListModel.fromJson(jsonData);
+
+      return blogsObject.blogs;
     } else {
       // Request failed
-      print('Request failed with status code: ${response.statusCode}');
       print('Response data: ${response.body}');
+      return Future.error('Request failed with status code: ${response.statusCode}');
     }
   } catch (e) {
     // Handle any errors that occurred during the request
