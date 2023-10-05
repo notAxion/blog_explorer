f1c0655aec7236f9465098feedade97521183863
notAxion <mr.bhakat456@gmail.com>
2023-10-05 12:31:28 +0530

    feat(ChangeNotifier): changing isFavorite will notify listerners

--- lib/models/blogs_model.dart
+++ lib/models/blogs_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'blogs_model.g.dart';
@@ -15,14 +16,22 @@ class BlogsListModel {
 }
 
 @JsonSerializable(createToJson: false)
-class BlogModel {
+class BlogModel with ChangeNotifier {
   // uuid
   final String id;
   @JsonKey(name: "image_url")
   final String imageUrl;
   final String title;
   @JsonKey(includeFromJson: false)
-  bool isFavorite;
+  bool _isFavorite;
+
+  bool get isFavorite => _isFavorite;
+
+  set isFavorite(bool value) {
+    _isFavorite = value;
+    notifyListeners();
+  }
+
   @JsonKey(includeFromJson: false)
   bool isHidden;
 
@@ -30,9 +39,9 @@ class BlogModel {
     required this.id,
     required this.imageUrl,
     required this.title,
-    this.isFavorite = false,
+    bool isFavorite = false,
     this.isHidden = false,
-  });
+  }) : _isFavorite = isFavorite;
 
   factory BlogModel.fromJson(Map<String, dynamic> json) =>
       _$BlogModelFromJson(json);
